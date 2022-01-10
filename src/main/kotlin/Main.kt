97596bcda4c0d4e0e49684dd831bd5d1264d3a77
Henry Bogaeus <henryb@spotify.com>
2022-01-10 16:56:08 +0100

    Ensure database file exists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,21 @@ import com.typesafe.config.ConfigFactory
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.before
 import io.javalin.apibuilder.ApiBuilder.crud
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.pathString
 
 fun main() {
     val config = ConfigFactory.load()
-    val databaseFilePath = config.getString("database_file")
+    val databaseFilePath = Path.of(config.getString("database_file"))
     val authorizationSecret = config.getString("authorization_secret")
     val port = config.getInt("port")
 
-    val itemStore = DatabaseArchiveItemStore("jdbc:sqlite:$databaseFilePath")
+    if (!Files.exists(databaseFilePath)) {
+        throw IllegalStateException("Database file does not exist at '${databaseFilePath.toAbsolutePath()}'")
+    }
+
+    val itemStore = DatabaseArchiveItemStore("jdbc:sqlite:${databaseFilePath.pathString}")
     val archiveItemController = ArchiveItemController(itemStore)
 
     val authorizationHeaderChecker = AuthorizationHeaderChecker(authorizationSecret)
